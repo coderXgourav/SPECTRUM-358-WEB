f261dd48980e088c7a9e8bc50ad9367a305ff367
TushankarKryptronix <tushankar1.kyptronix@outlook.com>
2025-08-19 14:17:29 +0530

    feat: Configure Firebase using environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBGeLzXJ46Lcuu23j8X6VWapYkaSkkhklk",
-  authDomain: "spectrum358-b18c5.firebaseapp.com",
-  projectId: "spectrum358-b18c5",
-  storageBucket: "spectrum358-b18c5.appspot.com",
-  messagingSenderId: "311252039544",
-  appId: "1:311252039544:web:39d62a5966eecaeec37a8d",
-  measurementId: "G-0SRWM45ZV4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
